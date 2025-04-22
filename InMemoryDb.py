d4173ee796e93b305eefa5cd80971262cacaee59
ronan.virmani <ronan.virmani@ufl.edu>
2025-04-21 22:50:05 -0400

    third

--- InMemoryDb.py
+++ InMemoryDb.py
@@ -31,38 +31,3 @@ class InMemoryDB:
 
     def get(self, key):
         return self.commited.get(key)
-
-if __name__ == "__main__":
-    db = InMemoryDB()
-    print(db.get("A"))  # None
-
-    try:
-        db.put("A", 5)
-    except RuntimeError as err:
-        print("Error:", err)
-
-    db.begin_transaction()
-    db.put("A", 5)
-    print(db.get("A"))  # Still None (uncommitted)
-    db.put("A", 6)
-    db.commit()
-    print(db.get("A"))  # 6
-
-    for fn in (db.commit, db.rollback):
-        try:
-            fn()
-        except RuntimeError as err:
-            print("Error:", err)
-
-    print(db.get("B"))  # None
-
-    db.begin_transaction()
-    db.put("B", 10)
-    db.rollback()
-    print(db.get("B"))  # None
-
-    db2 = InMemoryDB()
-    db2.begin_transaction()
-    db2.put("X", 99)
-    db2.commit()
-    print(db2.get("X"))  # 99
